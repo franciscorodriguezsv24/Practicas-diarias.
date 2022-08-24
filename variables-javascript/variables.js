4ec9837597e36c8feee87a7ae39694d29feb8334
Francisco Rodriguez <franciscorodriguezsv24@gmail.com>
2022-08-23 19:08:45 -0600

    Variables.

---
+++ variables-javascript/variables.js
@@ -0,0 +1,13 @@
+var unerName = " Juan";
+let variableNombre = "Hola mi nombre es";
+
+
+var curso = " Full stack junior";
+let variableCurso = ", estoy en el curso";
+
+
+var hobbie = " Jugar videojuegos, escuchar musica, bailar y sobre todo programar.";
+let variableHobbie = " y lo que más me apasiona es.";
+
+
+console.log(variableNombre +  unerName + variableCurso + curso + variableHobbie + hobbie )
