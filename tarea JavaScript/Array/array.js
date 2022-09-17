d3dc2a7838ea4ad35a8aeec7522d8b97923daa19
Francisco Rodriguez <franciscorodriguezsv24@gmail.com>
2022-09-16 20:25:07 -0600

    proyectos bootcamp

---
+++ tarea JavaScript/Array/array.js
@@ -0,0 +1,20 @@
+ let array = [2,5,7,9,12];
+
+ var arrayPrimero = "Este es el primer numero de tu array: "
+
+ var arraySegundo = "Este es el segundo numero de tu array: "
+
+ var arrayTercero = "Este es el tercer numero de tu array: "
+
+ var arrayCuarto = "Este es el cuarto numero de tu array: "
+
+ var arrayQuinto = "Este es el quinto numero de tu array: "
+
+
+
+ console.log(arrayPrimero + array [0]);
+
+console.log(arraySegundo + array [1]);
+console.log(arrayTercero + array [2]);
+console.log(arrayCuarto + array [3]);
+console.log(arrayQuinto + array [4]);
